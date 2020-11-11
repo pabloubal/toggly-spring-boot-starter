f19a2c3b7c1126b0773985e1f878123348f62d0b
Pablo Ubal <pablo.ubal@flexit.es>
2020-11-11 05:10:46 +0100

    chore:fix default configuration

--- src/main/java/io/github/pabloubal/togglysbstarter/core/configuration/TogglyConfiguration.java
+++ src/main/java/io/github/pabloubal/togglysbstarter/core/configuration/TogglyConfiguration.java
@@ -10,28 +10,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "toggly")
 public class TogglyConfiguration {
-  GlobalConfig global;
-  ScrapperConfig scrapper;
-  Controller controller;
+  GlobalConfig global = new GlobalConfig();
+  ScrapperConfig scrapper = new ScrapperConfig();
+  Controller controller = new Controller();
 
   @Data
   @NoArgsConstructor
   public static class GlobalConfig {
-    boolean enabledByDefault;
-    boolean missingTogglesBackToDefault;
+    boolean enabledByDefault = TogglyDefaultConfigurations.GLOBALCONFIG_ENABLEDBYDEFAULT;
+    boolean missingTogglesBackToDefault = TogglyDefaultConfigurations.GLOBALCONFIG_MISSINGTOGGLESBACKTODEFAULT;
   }
 
   @Data
   @NoArgsConstructor
   public static class ScrapperConfig {
-    Integer rate;
+    Integer rate = TogglyDefaultConfigurations.SCRAPPERCONFIG_RATE;
 
   }
 
   @Data
   @NoArgsConstructor
   public static class Controller {
-    String baseUrl;
+    String baseUrl = TogglyDefaultConfigurations.CONTROLLER_BASEURL;
   }
 
 }
---
+++ src/main/java/io/github/pabloubal/togglysbstarter/core/configuration/TogglyDefaultConfigurations.java
@@ -0,0 +1,8 @@
+package io.github.pabloubal.togglysbstarter.core.configuration;
+
+public abstract class TogglyDefaultConfigurations {
+  public static final boolean GLOBALCONFIG_ENABLEDBYDEFAULT = true;
+  public static final boolean GLOBALCONFIG_MISSINGTOGGLESBACKTODEFAULT = true;
+  public static final Integer SCRAPPERCONFIG_RATE = 60000;
+  public static final String CONTROLLER_BASEURL = "http://localhost:4000";
+}
